17dbf1703c9f41c9c8a67537dcbe52d821a1266b
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-08-11 09:45:01 +0200

    Update `Navbar` links to use proper routes and enhance `Video` component with autoplay, loop, and muted attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
       <section className="video">
           <div>
-              <Video src={StockVideoFootage}></Video>
+              <Video src={StockVideoFootage} loop muted autoplay/>
           </div>
           <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
           </div>
