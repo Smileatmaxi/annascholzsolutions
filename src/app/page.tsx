d2c3379ca4f65cf706582b4d36a71b97893c845a
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-08-11 09:21:23 +0200

    Add `privacy-policy` and `terms-and-conditions` pages, integrate stock video on the home page using `next-video`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Video from "next-video"
+import StockVideoFootage from "/videos/StockVideoMainPage.mp4"
 
 export default function Home() {
   return (
       <section className="video">
           <div>
+              <Video src={StockVideoFootage}></Video>
           </div>
           <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
           </div>
