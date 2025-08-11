67126f60ee413edd74937483569f8ed5e10ff6af
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-08-11 09:06:29 +0200

    Add new pages (`pricing`, `contact`, `about`, `services`), integrate `next-video`, update footer metadata, and replace outdated social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import React from "react";
+import Video from "next-video"
 
 export default function Home() {
   return (
-      <section className="">
+      <section className="video">
+          <div>
+          </div>
           <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-
           </div>
       </section>
   );
